55154e314fad80dc5e57db780e638fd53923bf73
LindesFeet <vicekings2021@gmail.com>
2025-06-13 01:54:21 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+lua54 'maybe'
+fxversion 'cerulean'
+game 'gta5'
+
+author 'urmomlikesdicknballs'
+description 'Qbus but in 2026 and revived better then ever'
+version '∅.3.1.4'
+
+client_scripts {
+    'init.lua',
+}
+
